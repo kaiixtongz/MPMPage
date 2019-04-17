ee03c12d34ed7ed86c6aa47d21165b4f062502f2
Tong Thammanawakun <kaiixtongz@gmail.com>
2019-04-17 13:41:37 +0700

    17-Apr-19 MySQLi

--- db_mpmpage.sql
+++ db_mpmpage.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 12, 2019 at 04:09 PM
+-- Generation Time: Apr 17, 2019 at 01:39 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.0
 
@@ -46,9 +46,10 @@ CREATE TABLE `customer` (
 INSERT INTO `customer` (`customerId`, `customerName`, `customerTel`, `customerEmail`, `customerAddress`, `customerConnect`, `customerStatus`, `customerTimelog`) VALUES
 (1, 'นภัสสร พวงไทย', '0944930751', 'mild_napassorn@outlook.com', '45/191 ม.5 ต.ท่าข้าม อ.สามพราน จ.นครปฐม 73110', '868189996689324', 1, '2019-02-24 09:20:34'),
 (2, 'รติพร พงษ์วดี', '0854049446', 'por.ritiporn@hotmail.com', '11/105 ม.5 ', '868189996689324', 1, '2019-03-03 03:20:21'),
-(3, 'โต้ง', '0000000000', 'tong@gmail.com', '456/123', '350348718897021', 1, '2019-02-24 07:33:24'),
-(4, 'โต้ง', '0809073005', 'tong@gmail', '66/33', '868189996689324', 2, '2019-02-24 09:31:28'),
-(5, 'อดิพงษ์ ธรรมนวกุล', '0809073005', 'tong.adipong@gmail.com', '200 ถ.พุทธมณฑลสาย3 แขวงบางไผ่ เขตบางแค กรุงเทพฯ 10160', '868189996689324', 1, '2019-03-03 03:26:42');
+(5, 'อดิพงษ์ ธรรมนวกุล', '0809073005', 'tong.adipong@gmail.com', '200 ถ.พุทธมณฑลสาย3 แขวงบางไผ่ เขตบางแค กรุงเทพฯ 10160', '868189996689324', 1, '2019-03-03 03:26:42'),
+(6, 'อดิศักดิ์ ธรรมนวกุล', '0819213032', 'song.pk@hotmail.com', '198 ถนน.พุทธมณฑลสาย 3 แขวง.บางไผ่ เขต.บางแค กรุงเทพฯ 10160', '350348718897021', 1, '2019-04-17 03:04:23'),
+(7, 'อดิศร ธรรมนวกุล', '0818165394', 'kang.adisorn@hotmail.com', '204 ถนน.พุทธมณฑลสาย 3 แขวง.บางไผ่ เขต.บางแค กรุงเทพฯ 10160', '350348718897021', 1, '2019-04-17 03:06:15'),
+(8, 'อดิพงษ์ ธรรมนวกุล', '0809073005', 'tong.adipong@gmail.com', '200 ถนน.พุทธมณฑลสาย 3 แขวง.บางไผ่ เขต.บางแค กรุงเทพฯ 10160', '350348718897021', 1, '2019-04-17 03:06:36');
 
 -- --------------------------------------------------------
 
@@ -77,7 +78,11 @@ INSERT INTO `express` (`expressId`, `expressName`, `expressPrice`, `expressDetai
 (3, 'Alpha', '30', 'ลงทะเบียน', 'alpha.png', '868189996689324', 1, '2019-03-14 05:22:30'),
 (4, 'Nim Express', '90', 'ส่งดวน', 'nimexpress.png', '868189996689324', 1, '2019-03-14 05:22:50'),
 (5, 'Grab Express', '-', 'ตามระยะทาง', 'grab.png', '868189996689324', 1, '2019-03-14 05:23:15'),
-(6, 'Lineman', '-', 'ตามระยะทาง', 'lineman.png', '868189996689324', 1, '2019-03-14 05:24:07');
+(6, 'Lineman', '-', 'ตามระยะทาง', 'lineman.png', '868189996689324', 1, '2019-03-14 05:24:07'),
+(7, 'ไปรษณีย์ไทย', '50', 'ลงทะเบียน', 'thaipost.png', '350348718897021', 1, '2019-04-17 03:14:20'),
+(8, 'ไปรษณีย์ไทย', '100', 'ส่งด่วน (EMS)', 'thaipost.png', '350348718897021', 1, '2019-04-17 03:14:33'),
+(9, 'Kerry', '150', 'ส่งด่วน (EMS)', 'kerry.png', '350348718897021', 1, '2019-04-17 03:14:45'),
+(10, 'Kerry', '200', 'เก็บเงินปลายทาง (COD)', 'kerry.png', '350348718897021', 1, '2019-04-17 03:15:46');
 
 -- --------------------------------------------------------
 
@@ -87,6 +92,7 @@ INSERT INTO `express` (`expressId`, `expressName`, `expressPrice`, `expressDetai
 
 CREATE TABLE `order` (
   `orderId` int(11) NOT NULL,
+  `orderNo` varchar(100) NOT NULL,
   `customerId` varchar(50) NOT NULL,
   `expressId` varchar(50) NOT NULL,
   `orderTotal` varchar(50) NOT NULL,
@@ -95,6 +101,13 @@ CREATE TABLE `order` (
   `orderStatus` int(11) NOT NULL DEFAULT '1',
   `orderTimeLog` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `order` (`orderId`, `orderNo`, `customerId`, `expressId`, `orderTotal`, `orderDiscount`, `orderConnect`, `orderStatus`, `orderTimeLog`) VALUES
+(1, 'QT-190417-113443', '8', '7', '38000', '470', '350348718897021', 2, '2019-04-17 06:31:23');
 
 -- --------------------------------------------------------
 
@@ -110,6 +123,16 @@ CREATE TABLE `order_product` (
   `order_productStatus` int(11) NOT NULL DEFAULT '1',
   `order_productTimeLog` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `order_product`
+--
+
+INSERT INTO `order_product` (`order_productId`, `productId`, `productValue`, `orderId`, `order_productStatus`, `order_productTimeLog`) VALUES
+(1, '23', '1', '1', 1, '2019-04-17 04:10:19'),
+(2, '24', '1', '1', 1, '2019-04-17 04:10:20'),
+(3, '25', '1', '1', 1, '2019-04-17 04:10:21'),
+(4, '26', '1', '1', 1, '2019-04-17 04:10:22');
 
 -- --------------------------------------------------------
 
@@ -170,24 +193,19 @@ INSERT INTO `product` (`productId`, `productName`, `productPrice`, `productDetai
 (2, 'HP OMEN', '40000', '', '20190309133335.jpg', '868189996689324', 8, 1, '2019-04-12 14:02:39'),
 (3, 'Alien', '69000', '', '20190309133613.jpg', '868189996689324', 8, 1, '2019-04-12 14:01:20'),
 (4, 'Mini Notebook', '14590', '', '20190309133751.jpg', '868189996689324', 8, 1, '2019-04-12 14:02:27'),
-(5, 'ทดสอบ', '12345', '', '20190309134711.jpg', '868189996689324', 8, 1, '2019-04-12 14:02:29'),
-(6, 'ทดสอบ 2', '98', '', '20190309134924.jpg', '868189996689324', 8, 1, '2019-04-12 14:02:26'),
-(7, 'ทดสอบ', '12356', '', '20190309142716.jpg', '868189996689324', 8, 1, '2019-04-12 14:02:30'),
 (8, 'Samsung', '37500', '', '20190325095224.jpg', '868189996689324', 6, 1, '2019-04-12 14:02:31'),
 (9, 'Van', '1990', '', '20190325094841.jpg', '868189996689324', 3, 1, '2019-04-12 14:02:32'),
 (10, '4x6 นิ้ว', '15', '', 'noImage.png', '868189996689324', 1, 1, '2019-03-09 13:38:27'),
 (11, 'Snap', '3', '', 'noImage.png', '868189996689324', 1, 1, '2019-04-12 14:02:34'),
-(12, '...', '123', '', 'noImage.png', '868189996689324', 3, 2, '2019-04-12 14:02:35'),
-(13, 'ทดสอบ', '15455', '', 'noImage.png', '868189996689324', 8, 2, '2019-03-25 09:25:07'),
-(14, 'LG', '45000', '', '20190325095855.jpg', '868189996689324', 6, 2, '2019-04-12 14:02:54'),
-(15, 'Hitachi', '29999', '8000 BTU', '20190325100414.jpg', '350348718897021', 7, 1, '2019-04-12 14:02:51'),
-(16, 'vannn', '1999', '', '20190326073756.jpg', '868189996689324', 3, 2, '2019-04-12 14:02:55'),
-(17, 'ทดสอบ', '1236', '', 'noImage.png', '868189996689324', 3, 2, '2019-04-12 14:02:56'),
 (18, 'สมุดเล่มเล็ก', '39', '', 'noImage.png', '868189996689324', 9, 1, '2019-04-12 14:02:57'),
 (19, 'Converse Jack Purcell', '2200', '', '20190405060737.jpg', '868189996689324', 3, 1, '2019-04-12 14:02:57'),
 (20, 'Adidas', '2300', '', '20190405060840.jpg', '868189996689324', 3, 1, '2019-04-12 14:02:59'),
 (21, 'adidas', '2400', '', '20190405060936.jpg', '868189996689324', 3, 1, '2019-04-12 14:03:00'),
-(22, 'converse star', '1690', '', '20190405061046.jpg', '868189996689324', 3, 1, '2019-04-12 14:03:01');
+(22, 'converse star', '1690', '', '20190405061046.jpg', '868189996689324', 3, 1, '2019-04-12 14:03:01'),
+(23, 'Sony WH-1000XM3', '13990', 'Bluetooth	• Bluetooth 4.2 / USB-C', '20190417045325.jpg', '350348718897021', 10, 1, '2019-04-17 02:53:25'),
+(24, 'Beats Studio3 Wireless (Blue)', '12500', 'Bluetooth	• Bluetooth ล่าสุด / Micro USB	• ชาร์จไฟผ่าน Micro USB / AUX 3.5mm', '20190417045656.jpg', '350348718897021', 10, 1, '2019-04-17 02:56:56'),
+(25, 'Sony MDR-XB950B1 (Red)', '5990', 'Bluetooth	• Bluetooth ล่าสุด / AUX 3.5 mm / Micro USB', '20190417050123.jpg', '350348718897021', 10, 1, '2019-04-17 03:01:23'),
+(26, 'JBL E55BT (White)', '5990', 'Bluetooth	• Bluetooth ล่าสุด  / AUX 3.5 mm / Micro USB', '20190417050310.png', '350348718897021', 10, 1, '2019-04-17 03:03:10');
 
 -- --------------------------------------------------------
 
@@ -211,12 +229,10 @@ INSERT INTO `product_group` (`productGroupId`, `productGroupName`, `productGroup
 (1, 'รูป', '868189996689324', 1, '2019-03-23 15:18:09'),
 (2, 'กระเป๋า', '868189996689324', 1, '2019-02-24 02:39:03'),
 (3, 'รองเท้า', '868189996689324', 1, '2019-02-24 02:39:14'),
-(4, 'โต๊ะ', '868189996689324', 2, '2019-03-25 09:45:48'),
-(5, 'ไฟ', '868189996689324', 2, '2019-03-03 16:21:56'),
 (6, 'ทีวี', '868189996689324', 1, '2019-02-24 06:58:11'),
-(7, 'แอร์', '350348718897021', 1, '2019-02-24 07:32:37'),
 (8, 'คอมพิวเตอร์', '868189996689324', 1, '2019-03-09 12:09:05'),
-(9, 'สมุด', '868189996689324', 1, '2019-03-28 09:24:22');
+(9, 'สมุด', '868189996689324', 1, '2019-03-28 09:24:22'),
+(10, 'หูฟัง', '350348718897021', 1, '2019-04-17 02:49:31');
 
 --
 -- Indexes for dumped tables
@@ -272,25 +288,25 @@ ALTER TABLE `product_group`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `customerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `customerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `express`
 --
 ALTER TABLE `express`
-  MODIFY `expressId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `expressId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
-  MODIFY `orderId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `orderId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `order_product`
 --
 ALTER TABLE `order_product`
-  MODIFY `order_productId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `order_productId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `payment`
@@ -302,13 +318,13 @@ ALTER TABLE `payment`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `productId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `productId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `product_group`
 --
 ALTER TABLE `product_group`
-  MODIFY `productGroupId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `productGroupId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
